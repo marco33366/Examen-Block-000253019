1df363fefa5b6eb55089b6d3d76f9224a85cf220
marco33366 <marco33366@gmail.com>
2026-05-16 20:17:57 -0500

    Parte 3

--- contracts/universidad000253019.sol
+++ contracts/universidad000253019.sol
@@ -9,23 +9,32 @@ contract Universidad000253019 {
         uint256 id;
         string nombre;
         uint256 edad;
+        bool estado; // Atributo booleano añadido
     }
 
     Estudiante[] public estudiantes;
     address public dirContrato;
 
-    constructor() {
+
+    modifier registrarEjecutor() {
         console.log("Ejecutado por: 000253019 - Marco Armando Zapata Sotomayor");
+        _;
+    }
+
+    constructor() registrarEjecutor {
         dirContrato = address(this);
     }
 
-    function agregarElemento(uint256 _id, string memory _nombre, uint256 _edad) public {
-        estudiantes.push(Estudiante(_id, _nombre, _edad));
+    function agregarElemento(uint256 _id, string memory _nombre, uint256 _edad) public registrarEjecutor {
+        for (uint256 i = 0; i < estudiantes.length; i++) {
+            require(estudiantes[i].id != _id, "El ID ya existe");
+        }
+        require(_edad > 0, "La edad debe ser mayor a 0");
+
+        estudiantes.push(Estudiante(_id, _nombre, _edad, true));
     }
 
-    function contarElementos() public view returns (uint256) {
-        console.log("Ejecutado por: 000253019 - Marco Armando Zapata Sotomayor");
+    function contarElementos() public view registrarEjecutor returns (uint256) {
         return estudiantes.length;
     }
-    //
 }
